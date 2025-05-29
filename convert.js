c738f1213ea19d9b651e5a44ea8e13c315a92cdc
mrafiiathallah <athallahrafi181@gmail.com>
2025-05-29 20:45:31 +0700

    perubahan firebase.ts

--- convert.js
+++ convert.js
@@ -3,14 +3,17 @@ const fs = require("fs");
 // Baca file JSON
 const serviceAccount = require("./serviceAccountKey.json");
 
+// Escape \n untuk digunakan di Railway (jadi \\n)
+const privateKeyForRailway = serviceAccount.private_key.replace(/\n/g, "\\\\n");
+
 // Escape \n di private key
-const privateKey = serviceAccount.private_key.replace(/\n/g, "\\n");
+const privateKeyForEnv = serviceAccount.private_key.replace(/\n/g, "\\n");
 
 // Susun isi .env
 const envData = `
 FIREBASE_PROJECT_ID=${serviceAccount.project_id}
 FIREBASE_CLIENT_EMAIL=${serviceAccount.client_email}
-FIREBASE_PRIVATE_KEY="${privateKey}"
+FIREBASE_PRIVATE_KEY="${privateKeyForEnv}"
 `.trim();
 
 // Tulis ke file .env
