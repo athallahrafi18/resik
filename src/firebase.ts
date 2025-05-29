3f9e3e9389cbf193cd9c6fcd6f0564838ee04795
mrafiiathallah <athallahrafi181@gmail.com>
2025-05-29 21:17:30 +0700

    tambahin log firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,6 +3,12 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+// Debug
+console.log("🔥 ENV DEBUG:");
+console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
+console.log("FIREBASE_CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
+console.log("FIREBASE_PRIVATE_KEY exists?", !!process.env.FIREBASE_PRIVATE_KEY);
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
